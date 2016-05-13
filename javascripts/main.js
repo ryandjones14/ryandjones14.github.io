16dab0ab5d3e716f22a2725ee7817a04b07ef41e
Ryan Jones <ryandjones14@gmail.com>
2016-05-12 22:55:19 -0500

    started working on layout for projects and added about div

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,19 @@
 $(function(){
-  $('.project').hover(function() {
-    $(this).children().children('p').toggleClass('hide');
+
+  $('.project').mouseenter(function() {
+    $(this).children('.project__text').toggleClass('hide');
+  });
+  $('.project').mouseleave(function() {
+    $(this).children('.project__text').toggleClass('hide');
+  });
+
+  $('.menu__about').on('click', function() {
+    $('.about').removeClass('hide');
+    $('.about').siblings().addClass('hide');
+  });
+
+  $('.menu__projects').on('click', function() {
+    $('.projects').removeClass('hide');
+    $('.projects').siblings().addClass('hide');
   });
 })
