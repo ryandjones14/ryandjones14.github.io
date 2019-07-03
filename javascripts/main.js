cc530d76630e7757d01dfab2e09b02d73c1cf395
Ryan Jones <ryandjones14@gmail.com>
2019-07-03 01:09:10 -0500

    total website redesign

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,43 +1,7 @@
-$(function(){
-
-// Code to pop up text for projects
-
-  $('article').mouseenter(function() {
-    $(this).children('.project__text').toggleClass('hide').toggleClass('show');
-  });
-  $('article').mouseleave(function() {
-    $(this).children('.project__text').toggleClass('show').toggleClass('hide');
-  });
-
-  // Code to show which section someone is one in header
-
-  $('.menu__about').on('click', function() {
-    $(this).siblings().removeClass('active');
-    $(this).addClass('active');
-    $('.about').removeClass('hide');
-    $('.about').siblings().addClass('hide');
-  });
-
-  $('.menu__projects').on('click', function() {
-    $(this).siblings().removeClass('active');
-    $(this).addClass('active');
-    $('.projects').removeClass('hide');
-    $('.projects').siblings().addClass('hide');
-  });
-
-  // Code to minimize header
-
-  $(window).scroll( function() {
-      var value = $(this).scrollTop();
-      if ( value > 200 ) {
-        $('.header').addClass('header2');
-      } else {
-        $('.header').removeClass('header2');
-      }
-  });
-
-  // Code to animate scrolling
-
-  $.localScroll({duration:800});
-
-})
+document.addEventListener('click', function (event) {
+  if (!event.target.matches('.solution-letter')) return;
+  event.preventDefault();
+  const letterClicked = event.target.innerHTML;
+  const idToChange = letterClicked.toUpperCase();
+  document.getElementById(idToChange).textContent = letterClicked;
+}, false);
