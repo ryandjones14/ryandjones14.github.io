113dfa86804302a5354be9373dd6846086b9a5d5
Ryan Jones <ryandjones14@gmail.com>
2019-07-04 17:26:58 -0500

    letters bounce once each

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
+  bounce();
   document.addEventListener('click', function (event) {
     if (!event.target.matches('.solution-letter')) return;
     event.preventDefault();
     const letterClicked = event.target.innerHTML;
     const idToChange = letterClicked.toUpperCase();
     document.getElementById(idToChange).textContent = letterClicked;
+    bounce();
   }, false);
+
   document.addEventListener('touchstart', function (event) {
     if (!event.target.matches('.solution-letter')) return;
     event.preventDefault();
@@ -13,4 +16,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const idToChange = letterClicked.toUpperCase();
     document.getElementById(idToChange).textContent = letterClicked;
   }, false);
+
+  function bounce() {
+    const solLetters = ['b', 'a', 'g', 'e', 'l'];
+    let index = Math.floor(Math.random() * 5);
+    let randomLetter = solLetters[index];
+    solLetters.splice(index, 1);
+    if (solLetters.length > 0) {
+      setTimeout(() => {
+        const el = document.getElementById(randomLetter);
+        console.log(el);
+        el.className += ' bounce';
+      }, 7394);
+    }
+  };
 });
