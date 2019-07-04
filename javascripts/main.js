38d84d1946a1c68914138fa45d10c5db07147eb3
Ryan Jones <ryandjones14@gmail.com>
2019-07-04 16:17:38 -0500

    design changes, adds touch event handler

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,16 @@
-document.addEventListener('click', function (event) {
-  if (!event.target.matches('.solution-letter')) return;
-  event.preventDefault();
-  const letterClicked = event.target.innerHTML;
-  const idToChange = letterClicked.toUpperCase();
-  document.getElementById(idToChange).textContent = letterClicked;
-}, false);
+document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener('click', function (event) {
+    if (!event.target.matches('.solution-letter')) return;
+    event.preventDefault();
+    const letterClicked = event.target.innerHTML;
+    const idToChange = letterClicked.toUpperCase();
+    document.getElementById(idToChange).textContent = letterClicked;
+  }, false);
+  document.addEventListener('touchstart', function (event) {
+    if (!event.target.matches('.solution-letter')) return;
+    event.preventDefault();
+    const letterClicked = event.target.innerHTML;
+    const idToChange = letterClicked.toUpperCase();
+    document.getElementById(idToChange).textContent = letterClicked;
+  }, false);
+});
