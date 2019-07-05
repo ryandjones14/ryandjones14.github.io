3645561b5eccad62d7abb326ad6353cd0f0fc584
Ryan Jones <ryandjones14@gmail.com>
2019-07-05 16:13:56 -0500

    refactor letter bounce func

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,33 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
   bounce();
   document.addEventListener('click', function (event) {
-    if (!event.target.matches('.solution-letter')) return;
-    event.preventDefault();
-    const letterClicked = event.target.innerHTML;
-    const idToChange = letterClicked.toUpperCase();
-    document.getElementById(idToChange).textContent = letterClicked;
-    bounce();
+    handleClick(event);
   }, false);
 
   document.addEventListener('touchstart', function (event) {
-    if (!event.target.matches('.solution-letter')) return;
-    event.preventDefault();
-    const letterClicked = event.target.innerHTML;
-    const idToChange = letterClicked.toUpperCase();
-    document.getElementById(idToChange).textContent = letterClicked;
+    handleClick(event);
   }, false);
 
-  function bounce() {
-    const solLetters = ['b', 'a', 'g', 'e', 'l'];
-    let index = Math.floor(Math.random() * 5);
-    let randomLetter = solLetters[index];
-    solLetters.splice(index, 1);
-    if (solLetters.length > 0) {
-      setTimeout(() => {
-        const el = document.getElementById(randomLetter);
-        console.log(el);
-        el.className += ' bounce';
-      }, 7394);
-    }
-  };
 });
+
+function handleClick(event) {
+  if (!event.target.matches('.solution-letter')) return;
+  event.preventDefault();
+  const letterClicked = event.target.innerHTML;
+  document.getElementById(letterClicked).className = 'solution-letter';
+  const idToChange = letterClicked.toUpperCase();
+  document.getElementById(idToChange).textContent = letterClicked;
+  bounce(letterClicked);
+};
+
+function bounce(letter = '') {
+  let solLetters = ['b', 'a', 'g', 'e', 'l'];
+  if (letter) {
+    solLetters.splice(solLetters.indexOf(letter), 1);
+  }
+  let index = Math.floor(Math.random() * solLetters.length);
+  let randomLetter = solLetters[index];
+  if (solLetters.length > 0) {
+    setTimeout(() => {
+      const el = document.getElementById(randomLetter);
+      el.className += ' bounce';
+    }, 7394);
+  }
+};
